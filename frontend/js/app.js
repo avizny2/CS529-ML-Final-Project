78afe56e7654f747bdf58b98ac6a3fa14edb4e22
benza613 <benito.alvares@gmail.com>
2020-11-12 13:09:39 -0600

    updated ui minor changes

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,7 +2,7 @@
 /* Get or create the application global variable */
 var App = App || {};
 
-(function() {
+(function () {
 
   // setup the pointer to the scope 'this' variable
   let self = this;
@@ -10,8 +10,8 @@ var App = App || {};
   let modelSelector, datasetSelector, dRedSelector, classesSelector, visualizeButton;
   let classesCheckboxes = [];
   let customClassesList = [];
-  
-  App.init = function() {
+
+  App.init = function () {
     drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
     topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.getElementById('app-bar'));
     topAppBar.setScrollTarget(document.getElementById('main-content'));
@@ -22,9 +22,9 @@ var App = App || {};
     modelSelector = new mdc.select.MDCSelect(document.querySelector('#model-selector'));
     datasetSelector = new mdc.select.MDCSelect(document.querySelector('#dataset-selector'));
     dRedSelector = new mdc.select.MDCSelect(document.querySelector('#d-red-selector'));
-    
+
     classesSelector = new mdc.select.MDCSelect(document.querySelector('#classes-selector'));
-    for (let i = 1; i <=10; i++) {
+    for (let i = 1; i <= 10; i++) {
       let checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('#menu-selector-checkbox-' + i));
       classesCheckboxes.push(checkbox);
     }
@@ -53,118 +53,121 @@ var App = App || {};
   };
 
   /* Entry point of the application */
-  App.start = function() {
-    d3.json("https://balvar30.people.uic.edu/finalproj/data/sample_json5.json").then(function(data) {
+  App.start = function () {
+    d3.json("/data/sample_json5.json").then(function (data) {
 
-      console.log('n_layers',data.n_layers)
-    
+      console.log('n_layers', data.n_layers)
+
       var n_layers = data.n_layers;
       var n_classes = 10; //data.classes.length;
-      
+
       var plotdata = [];
-      for(let i = 0; i < n_classes; i++) {
+      for (let i = 0; i < n_classes; i++) {
         plotdata.push([]);
       }
-      
+
       for (let layer_i = 0; layer_i < n_layers; layer_i++) {
-          if(data['dataset_'+layer_i]){
-            let layer_data = data['dataset_'+layer_i];
-            for (let class_i = 0; class_i < n_classes; class_i++){
-              var temp_arr = []
-              if(layer_data[class_i]){
-                layer_data[class_i].forEach(datapoint => {
-                  temp_arr.push(datapoint)
-                });
-                plotdata[class_i].push(temp_arr)
-              }
+        if (data['dataset_' + layer_i]) {
+          let layer_data = data['dataset_' + layer_i];
+          for (let class_i = 0; class_i < n_classes; class_i++) {
+            var temp_arr = []
+            if (layer_data[class_i]) {
+              layer_data[class_i].forEach(datapoint => {
+                temp_arr.push(datapoint)
+              });
+              plotdata[class_i].push(temp_arr)
             }
           }
+        }
       }
       //console.log(plotdata);
-      var margin = {top: 1, right: 1, bottom: 1, left: 1};
-      
+      var margin = { top: 1, right: 1, bottom: 1, left: 1 };
+
       function gridData() {
         var data = new Array();
         var xpos = 1; //starting xpos and ypos at 1 so the stroke will show when we make the grid below
         var ypos = 1;
-        var width = ($(window).height()-150)/n_classes-1;
-        var height = ($(window).height()-150)/n_classes-1;
-      
+        var width = ($(window).height() - 150) / n_classes - 1;
+        var height = ($(window).height() - 150) / n_classes - 1;
+
         // iterate for rows 
         for (var row = 0; row < n_classes; row++) {
-            data.push( new Array() );
-      
-            // iterate for cells/columns inside rows
-            for (var column = 0; column < n_layers; column++) {
-                data[row].push({
-                    x: xpos,
-                    y: ypos,
-                    width: width,
-                    height: height
-                })
-                // increment the x position. I.e. move it over by 50 (width variable)
-                xpos += width;
-            }
-            // reset the x position after a row is complete
-            xpos = 1;
-            // increment the y position for the next row. Move it down 50 (height variable)
-            ypos += height; 
+          data.push(new Array());
+
+          // iterate for cells/columns inside rows
+          for (var column = 0; column < n_layers; column++) {
+            data[row].push({
+              x: xpos,
+              y: ypos,
+              width: width,
+              height: height
+            })
+            // increment the x position. I.e. move it over by 50 (width variable)
+            xpos += width;
+          }
+          // reset the x position after a row is complete
+          xpos = 1;
+          // increment the y position for the next row. Move it down 50 (height variable)
+          ypos += height;
         }
         return data;
       }
-      
+
       var gridData = gridData();
-      
+
       function PlotGrid(id, width, height, square) {
         //Xaxis
         var x = d3.scaleLinear()
-              .domain([-5, 5])
-              .range([ 0, 52]);
+          .domain([-5, 5])
+          .range([0, 52]);
 
         //Yaxis
         var y = d3.scaleLinear()
-        .domain([-4, 4])
-        .range([ 52, 0]);
+          .domain([-4, 4])
+          .range([52, 0]);
 
         var grid = d3.select(id).append("svg")
-          // .attr("width", $(window).height() )
+          .attr("width", "90vw")
           .attr("height", $(window).height() - 150)
           .attr("class", "chart");
-      
+
         var row = grid.selectAll(".row")
           .data(gridData)
           .enter().append("g")
-          .attr("class", "row");
-        
+          .attr("class", "row")
+          .style("margin", "15px");
+
         var col = row.selectAll(".column")
-          .data(function(d, i) {
+          .data(function (d, i) {
             let r = [];
             for (let j = 0; j < n_layers; j++) {
               let x = {
-                'dim' : d[j],
-                'data' : plotdata[i][j]
-                };
+                'dim': d[j],
+                'data': plotdata[i][j]
+              };
               r.push(x);
             }
             return r;
           })
           .enter()
           .append("g")
-          .attr("class","column")
-          
-          col.append("rect")
-          .attr("class","square")
-          .attr("x", function(d) { return d.dim.x; })
-          .attr("y", function(d) { return d.dim.y; })
-          .attr("width", function(d) { return d.dim.width; })
-          .attr("height", function(d) { return d.dim.height; })
+          .attr("class", "column")
+          .style("margin", "15px")
+          .style("padding", "15px");
+
+        col.append("rect")
+          .attr("class", "square")
+          .attr("x", function (d) { return d.dim.x; })
+          .attr("y", function (d) { return d.dim.y; })
+          .attr("width", function (d) { return d.dim.width; })
+          .attr("height", function (d) { return d.dim.height; })
           .style("fill", "#fff")
           .style("stroke", "#222");
 
-          col.selectAll('.column')
-          .data(function(d) { 
+        col.selectAll('.column')
+          .data(function (d) {
             for (let i = 0; i < d.data.length; i++) {
-              
+
               d.data[i].comp1 = d.dim.x + x(parseFloat(d.data[i].comp1));
               d.data[i].comp2 = d.dim.y + y(parseFloat(d.data[i].comp2));
             }
@@ -172,8 +175,8 @@ var App = App || {};
           })
           .enter()
           .append("circle")
-          .attr("cx", function (d) { return d.comp1; } )
-          .attr("cy", function (d) { return d.comp2; } )
+          .attr("cx", function (d) { return d.comp1; })
+          .attr("cy", function (d) { return d.comp2; })
           .attr("r", 1.5)
           .style("fill", "#69b3a2");
       }
@@ -186,7 +189,6 @@ var App = App || {};
   App.init();
   App.start();
 
-}) ();
+})();
 
 
-  
