7cc20bb03e2eda33bbf356acfcb7871f11a2c7f1
Alex Viznytsya <avizny2@uic.edu>
2020-11-12 08:24:08 -0600

    Refactor JS code.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,29 +1,39 @@
-const drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+"use strict";
+/* Get or create the application global variable */
+var App = App || {};
 
-const topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.getElementById('app-bar'));
-topAppBar.setScrollTarget(document.getElementById('main-content'));
-topAppBar.listen('MDCTopAppBar:nav', () => {
-  drawer.open = !drawer.open;
-});
+(function() {
 
-const modelSelector = new mdc.select.MDCSelect(document.querySelector('#model-selector'));
-const datasetSelector = new mdc.select.MDCSelect(document.querySelector('#dataset-selector'));
-const dRedSelector = new mdc.select.MDCSelect(document.querySelector('#d-red-selector'));
-const classesSelector = new mdc.select.MDCSelect(document.querySelector('#classes-selector'));
-const classesCheckboxes = [];
-for (let i = 1; i <=10; i++) {
-  let checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('#menu-selector-checkbox-' + i));
-  classesCheckboxes.push(checkbox);
-}
+  // setup the pointer to the scope 'this' variable
+  let self = this;
+  let drawer, topAppBar;
+  let modelSelector, datasetSelector, dRedSelector, classesSelector, visualizeButton;
+  let classesCheckboxes = [];
+  let customClassesList = [];
+  
+  App.init = function() {
+    drawer = mdc.drawer.MDCDrawer.attachTo(document.querySelector('.mdc-drawer'));
+    topAppBar = mdc.topAppBar.MDCTopAppBar.attachTo(document.getElementById('app-bar'));
+    topAppBar.setScrollTarget(document.getElementById('main-content'));
+    topAppBar.listen('MDCTopAppBar:nav', () => {
+      drawer.open = !drawer.open;
+    });
 
-let customClassesList = [];
-classesSelector.listen('MDCSelect:change', () => {
-    if (classesSelector.selectedIndex === 0) {
-      return;
+    modelSelector = new mdc.select.MDCSelect(document.querySelector('#model-selector'));
+    datasetSelector = new mdc.select.MDCSelect(document.querySelector('#dataset-selector'));
+    dRedSelector = new mdc.select.MDCSelect(document.querySelector('#d-red-selector'));
+    
+    classesSelector = new mdc.select.MDCSelect(document.querySelector('#classes-selector'));
+    for (let i = 1; i <=10; i++) {
+      let checkbox = new mdc.checkbox.MDCCheckbox(document.querySelector('#menu-selector-checkbox-' + i));
+      classesCheckboxes.push(checkbox);
     }
-    customClassesList = [];
-    console.log("val: " + classesSelector.selectedIndex);
-    if (classesSelector.selectedIndex > 1) {
+    classesSelector.listen('MDCSelect:change', () => {
+      if (classesSelector.selectedIndex === 0) {
+        return;
+      }
+      customClassesList = [];
+      if (classesSelector.selectedIndex > 1) {
         classesSelector.selectedIndex = 0;
         for (let i = 0; i < 10; i++) {
           let checkbox = classesCheckboxes[i];
@@ -31,13 +41,25 @@ classesSelector.listen('MDCSelect:change', () => {
             customClassesList.push(i);
           }
         }
-    }
-});
+      }
+    });
+
+    visualizeButton = document.querySelector('#menu-visualize-button');
+    visualizeButton.addEventListener('click', (event) => {
+      drawer.open = false;
+      $('#visualization-section').html('');
+    });
+  };
+
+  /* Entry point of the application */
+  App.start = function() {
+      
+  };
+
+  App.init();
+  App.start();
+
+}) ();
 
 
-
-const visualizeButton = document.querySelector('#menu-button');
-visualizeButton.addEventListener('click', (event) => {
-  drawer.open = false;
-});
   
